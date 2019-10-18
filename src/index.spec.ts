201f6198cef8b7b581efad9228e530708a2041a1
ante <ante.kotarac@gmail.com>
2019-10-18 13:28:28 +0200

    first commit

---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { regionBoundMap } from './index';
+
+describe('on import regionBoundMap', () => {
+    test('should exist', () => {
+        expect(regionBoundMap).toBeTruthy();
+    });
+});
